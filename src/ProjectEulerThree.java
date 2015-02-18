2d2951bf340060d4980c20e0bbbdc7c22cb6c2f9
Danny Weng <danny.gl.weng@gmail.com>
2015-02-18 11:27:18 -1000

    Testing
    Project Euler Three

--- src/ProjectEulerThree.java
+++ src/ProjectEulerThree.java
@@ -3,18 +3,21 @@
  * Created by Weng on 2/2/2015.
  */
 public class ProjectEulerThree {
-  public static void main(String[] args){
+  /** Main method.
+   * @param args argument
+   */
+  public static void main(String[] args) {
 
     long targetNum = 600851475143L;
     long divider = 2;
     long lastPF = 0;
 
-    while(targetNum > 1){
-      while(targetNum%divider == 0){
+    while (targetNum > 1) {
+      while (targetNum % divider == 0) {
         System.out.print(divider + " ");
         lastPF = divider;
 
-        targetNum = targetNum/divider;
+        targetNum = targetNum / divider;
 
       }
       divider++;
